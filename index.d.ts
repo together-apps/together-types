b868a113b75f6d88a493a2ef2ff15b949b221a4e
Jakob Unnebäck <jakob@folkdatorn.se>
2023-08-01 12:03:14 +0200

    Remove completedBy

--- index.d.ts
+++ index.d.ts
@@ -84,7 +84,6 @@ export interface Session<FirebaseTimestamp> {
   agenda: Event[]
   authoredBy?: string
   categoryID: string
-  completedBy?: string[]
   createdBy: string
   currentEvent: OngoingEvent<FirebaseTimestamp> | 'waiting' | 'finished'
   description: string
