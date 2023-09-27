7acf188e5dcea9dd3115aa83ec4b400f974d6458
Jakob Unnebäck <jakob@folkdatorn.se>
2023-09-27 09:18:28 +0200

    Add templateID to reviews

--- index.d.ts
+++ index.d.ts
@@ -134,6 +134,7 @@ export interface Review<FirebaseTimestamp> {
   authorID: string
   comment: string
   createdAt: FirebaseTimestamp
+  templateID: string
 }
 
 // Reflections are private notes only visible to the author
